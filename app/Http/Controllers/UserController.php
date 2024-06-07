6e3c5b67a70acd4c8e782783be83f019c96083ca
snlkmr90@gmail.com <Tmnt200$>
2024-06-07 15:26:33 +0530

    used react-router-dom,axios etc

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(['data'=>User::get()]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8'
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['status'=>400,'errors'=>$validator->errors()]);
+        }
+         
+        $validated = $validator->validated();
+        $validated['password'] = Hash::make($validated['password']);
+        User::create($validated);
+        return response()->json(['status'=>200,'message'=>'User Created !']);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
